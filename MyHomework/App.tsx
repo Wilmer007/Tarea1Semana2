a1c62bfcc00fdfffb2a3eb53a93b5b8e6738285c
Wilmer007 <102242571+Wilmer007@users.noreply.github.com>
2025-10-31 19:36:09 -0600

    Avances iniciales

--- MyHomework/App.tsx
+++ MyHomework/App.tsx
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CustomInput from './src/Custominput';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [nombre, setNombre] = useState('Tu Nombre');
+  const [apellido, setApellido] = useState('Tu Apellido');
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Pon tu nombre y apellido</Text>
+      <CustomInput
+        placeholder="Nombre"
+        value={nombre}
+        setValue={() => {}}
+        onChange={setNombre}
+      />
+      <CustomInput
+        placeholder="Apellido"
+        value={apellido}
+        setValue={() => {}}
+        onChange={setApellido}
+      />
+      <Text>Hola {nombre} {apellido}</Text>
+
+
     </View>
   );
 }
