acc909b468719b790fcc26af17549c6b5c4d346c
Wilmer007 <102242571+Wilmer007@users.noreply.github.com>
2025-11-02 09:15:09 -0600

    Avance De Tarea

--- MyHomework/App.tsx
+++ MyHomework/App.tsx
@@ -1,30 +1,87 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import CustomInput from './src/Custominput';
-import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Alert, Button } from 'react-native';
+import { useState , useEffect} from 'react';
 
 export default function App() {
 
-  const [nombre, setNombre] = useState('Tu Nombre');
-  const [apellido, setApellido] = useState('Tu Apellido');
+  const [usuario, setUsuario] = useState('Tu usuario');
+  const [edad, setEdad] = useState('Tu edad');
+  const [count, setCount] = useState(0);
+  const [horaActual, setHoraActual] = useState('');
+
+  const incrementCount = () => {
+    setCount(count + 1);
+    if (count%5 === 0) {
+      alert(`Has alcanzado un multiplo de 5 : ${count}`);
+  }}
+  useEffect(() => {
+    if (count+1) {
+    console.log(`El contador a cambiado a: ${count}`);
+  }}, [count]);
+
+  useEffect(() => {
+ const sacarYeliminar = () => {
+
+    const hoy = new Date();
+    const hora = hoy.getHours();
+    const minutos = hoy.getMinutes();
+    const segundos = hoy.getSeconds();
+
+    const eliminarInterval = `${String(hora).padStart(2,"0")}:${String(minutos).padStart(2,"0")}:${String(segundos).padStart(2,"0")}`;
+    setHoraActual(eliminarInterval);
+
+ };
+
+ sacarYeliminar();
+
+ const timer = setInterval(sacarYeliminar, 1000);
+ return () => {
+    clearInterval(timer);
+ };
+  }, []);
+
+
+  const handleTextChange = (text: string) => {
+    const numericValue = text.replace(/[^0-9]/g, '');
+    setEdad(numericValue);
+  };
+
+
+  const handleSubmit = () => {
+    if (!usuario || usuario.trim().length === 0) {
+      Alert.alert('Validacion', 'porfavor ingresa usuario');
+      return;
+    }
+    if (!edad || edad.trim().length === 0) {
+      Alert.alert('Validacion', 'porfavor ingresa edad');
+      return;
+    }
+    Alert.alert('Enviado', `Hola ${usuario} tu edad es ${edad}`);
+  };
 
   return (
     <View style={styles.container}>
       <Text>Pon tu nombre y apellido</Text>
-      <CustomInput
-        placeholder="Nombre"
-        value={nombre}
-        setValue={() => {}}
-        onChange={setNombre}
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 3, marginBottom: 20, width: 200, paddingLeft: 10 }}
+        placeholder="ingresa Tu usuario"
+        value={usuario}
+        onChangeText={(value) => setUsuario(value)}
       />
-      <CustomInput
-        placeholder="Apellido"
-        value={apellido}
-        setValue={() => {}}
-        onChange={setApellido}
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 3, marginBottom: 20, width: 200, paddingLeft: 10 }}
+        placeholder="Solo numeros - Tu edad"
+        value={edad}
+        onChangeText={handleTextChange}
+        keyboardType='number-pad'
       />
-      <Text>Hola {nombre} {apellido}</Text>
+      <Button title="Enviar" onPress={handleSubmit} />
+      <Text>Hola {usuario} tu edad es {edad} </Text>
 
+      <Button title='Contador' onPress={incrementCount}></Button>
+      <Text>
+        Hora Actual: {horaActual}
+      </Text>
 
     </View>
   );
